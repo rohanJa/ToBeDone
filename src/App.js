b9812fbee7676eeed007645da769bacc8f145524
rohanJa <rohan.jain150300@gmail.com>
2020-06-06 17:04:48 +0530

    Component Added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-    
-    </div>
-  );
+import React,{Component} from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css"
+import CreateTodo from './components/CreateTodo'
+import EditTodo from './components/EditTodo'
+import TodosList from './components/TodosList'
+class App extends Component {
+  render(){
+    return (
+        <Router>
+          <div className="container">
+            <h2>MERN-Stack Todo App</h2>
+          </div>
+          <Route path="/" exact component={TodosList}/>
+          <Route path="/edit/:id" component={EditTodo}/>
+          <Route path="/create" component={CreateTodo}/>
+        </Router>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/EditTodo/index.js
@@ -0,0 +1,11 @@
+import React,{Component} from 'react';
+
+export default class EditTodo extends Component{
+    render(){
+        return(
+            <div>
+                <p>Welcome to Edit ToDo Component!!</p>
+            </div>
+        )
+    }
+}
